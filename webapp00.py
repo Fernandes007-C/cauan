e491e0d83f1b3025f46360b049ecac7016e8cf04
Fernandes007-C <cauanfernandes1510@gmail.com>
2024-08-08 10:03:42 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -4,7 +4,6 @@ import pandas as pd
 import streamlit as st
 
 st.title("Olá, Cauan!")
-
 st.header("Turma 2EI-S")
-          
 st.header("É a melhor turma!")         
+st.write("vamos lá pessoal!!!")
